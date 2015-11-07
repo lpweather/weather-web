65fb873351638f3bd02df9db1da97a7df2709da1
Eddex <eddex@outlook.com>
2015-11-07 05:16:41 +0100

    something is working

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 import os
 
 app = Flask(__name__)
+app.debug = True
 
 port = int(os.getenv("VCAP_APP_PORT") or 8080)
 print port
 
+app._static_folder = 'www/'
+
 @app.route('/')
 def index():
-    return 'Hello World'
+    return app.send_static_file('index.html')
+
+@app.route('/js/<path:path>')
+def send_js(path):
+    return send_from_directory('www/js/', path)
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port)
---
+++ www/js/app.js
@@ -0,0 +1,12 @@
+(function() {
+  var app = angular.module('weather', []);
+
+  app.controller('WeatherController', function(){
+    this.product = info;
+  });
+
+  var info = {
+    temperature: 45.69,
+    location: "47°22′N 8°33′E",
+  };
+})();
