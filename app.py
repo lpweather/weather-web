8e2c91ce1e1b01daff6963cdbd90555f8cdef4a4
Eddex <eddex@outlook.com>
2015-11-07 02:02:18 +0100

    Deploy app on cloud

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
+
+port = int(os.getenv("VCAP_APP_PORT") or 8080)
+print port
 
 @app.route('/')
 def index():
     return 'Hello World'
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', 8080)
+    app.run('0.0.0.0', port)
